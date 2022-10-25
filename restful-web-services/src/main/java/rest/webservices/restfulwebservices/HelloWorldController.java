aca8d4ff1090b5148963ce87681d4a820fda4281
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-10-25 22:42:39 +0530

    enhanced hello world service to return a bean

--- restful-web-services/src/main/java/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -12,9 +12,18 @@ public class HelloWorldController {
 	//URI -/hello-world
 	//method - "Hello World"
 	
+	
+	
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
 		return "Hello world";
+	}
+	
+	//returning beans
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean HelloWorldBean() {
+		return new HelloWorldBean("Hello world");
 	}
 
 }
