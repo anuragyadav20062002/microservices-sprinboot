596c1613b00050a898f4f439c835a23661052c79
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-10-25 22:28:51 +0530

    Update HelloWorldController.java

--- restful-web-services/src/main/java/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,7 +1,6 @@
 package rest.webservices.restfulwebservices;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //COntroller
@@ -13,7 +12,7 @@ public class HelloWorldController {
 	//URI -/hello-world
 	//method - "Hello World"
 	
-	@RequestMapping(method=RequestMethod.GET, path="/hello-world")
+	@GetMapping(path="/hello-world")
 	public String helloWorld() {
 		return "Hello world";
 	}
